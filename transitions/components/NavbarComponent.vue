6fe39fa8e35df2965e53ffe3e603542b80aec702
Zignuts Parth Gupta <parthg@zignuts.com>
2024-03-28 19:04:16 +0530

    added the transition folder into it

---
+++ transitions/components/NavbarComponent.vue
@@ -0,0 +1,8 @@
+<template>
+    <ul>
+        <li><NuxtLink to="/">home Page</NuxtLink></li>
+        <li><NuxtLink to="/about">about Page</NuxtLink></li>
+        <li><NuxtLink to="/contact">contact Page</NuxtLink></li>
+    </ul>
+    
+</template>
---
+++ transitions/pages/contact.vue
@@ -0,0 +1,4 @@
+<template>
+    <h1>Contact Page</h1>
+    <h2>hello - this is the contact page</h2>
+</template>
